957f7e6a25cfe66b7a4abe5728e742285d0aaa86
Prashun Chitrakar <prashuncool@gmail.com>
2017-12-05 20:21:42 +0545

    Factorial

---
+++ patterns/factorial.c
@@ -0,0 +1,26 @@
+#include<stdalign.h>
+#include<stdio.h>
+
+int factorial(int n);
+
+int main()
+{
+    int a,b;
+    printf("Enter the number to make factorial of : ");
+    scanf("%d", &a);
+    b=factorial(a);
+    printf("The factorial of %d is %d", a,b);
+    return 0;
+}
+
+int factorial(int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    else
+    {
+        return (n*factorial(n-1));
+    }
+}
