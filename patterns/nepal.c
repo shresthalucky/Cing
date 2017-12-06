9615b271f7cf37185b80a3c4520526f4dd2a700e
Prashun Chitrakar <prashuncool@gmail.com>
2017-12-06 13:25:11 +0545

    User can add custom text to print

--- patterns/nepal.c
+++ patterns/nepal.c
@@ -4,12 +4,15 @@ void main() {
 
 	int i,j;
   // char n[5] = {'N','E','P','A','L'};
-	char str[] = "THIS IS NEPAL";
+	//char str[] = "THIS IS NEPAL";
+	char str[1000];
+	printf("Enter Ant Character : ");
+	scanf ("%[^\n]%*c", str);
 	char *pointer;
 
 	pointer = str;
 
-	for (pointer[i]; i>=0; i--){
+	for (i=0; pointer[i]; i++){
 		for (j=0;j<=i;++j) {
 			printf("%c", pointer[j]);
 		}
