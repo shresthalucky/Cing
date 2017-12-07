638e5e4a5d8045534da14b1cb10695eec0ff9944
Lucky Shrestha <luckshrestha@gmail.com>
2017-12-07 16:44:08 +0545

    checking if digit

--- array/main.c
+++ array/main.c
@@ -1,39 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "functions.h"
 
 int main(){
-  int a[50], n, i, j, temp;
 
-  printf("How many ints?\n");
-  scanf("%d", &n);
+	int a[50], n, i, j, temp;
 
-  for (i = 0; i < n; i++){
-  	printf("Enter number %d: ", i+1);
-  	scanf("%d", &a[i]);
-  }
+	printf("How many ints?\n");
+	scanf("%d", &n);
 
-  for (i = 0; i < n; i++){
-  	for (j=i+1; j < n; j++){
-  		if (a[i] > a[j])
-  		{
-  			temp = a[i];
-  			a[i] = a[j];
-  			a[j] = temp;
-  		}
-  	}
-  }
+	if(isdigit(n)){
+		for (i = 0; i < n; i++){
+			printf("Enter number %d: ", i+1);
+			scanf("%d", &a[i]);
+		}
+		for (i = 0; i < n; i++){
+			for (j=i+1; j < n; j++){
+				if (a[i] > a[j])
+				{
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
 
-  printf("Ascending sorting: ");
-  for (i = 0; i < n; i++){
-  	printf("%d, ", a[i]);
-  }
+		printf("Ascending sorting: ");
+		for (i = 0; i < n; i++){
+			printf("%d, ", a[i]);
+		}
 
-  printf("Descending sorting: ");
-  for (i = n-1; i >= 0; i--){
-  	printf("%d, ", a[i]);
-  }
+		printf("Descending sorting: ");
+		for (i = n-1; i >= 0; i--){
+			printf("%d, ", a[i]);
+		}
+	}else {
+  		exit(EXIT_FAILURE); //same as exit(1)
+	}
+
 
   // aSum(n, a);
 
-  return 0;
+	return 0;
 }
