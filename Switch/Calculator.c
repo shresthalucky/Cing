40236bdda96475f24b6745e8be5156115ae89d4e
Prashun Chitrakar <prashuncool@gmail.com>
2017-12-07 10:02:11 +0545

    Edited Header File

--- Switch/Calculator.c
+++ Switch/Calculator.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "functions.h"
+#include "defination.h"
 
 // void add(int a, int b);
 // void sub(int a, int b);
@@ -7,7 +7,7 @@
 // void div(int a, int b);
 
 int main() {
-
+z
   int a, b, c;
 
   printf("Enter 2 numbers:");
