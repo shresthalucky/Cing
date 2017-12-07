2849d84192c2fb801f40c17fb3c220fa8c615f61
Prashun Chitrakar <prashuncool@gmail.com>
2017-12-07 10:17:02 +0545

    Program to count number of digits

---
+++ recrussion/digits-count.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int noOfDigits(int n);
+int main()
+{
+  int n,ctr;
+    printf("\n\n count the digits of a given number :\n");
+	printf("-----------------------------------------\n");
+    printf(" Input  a number : ");
+    scanf("%d",&n);
+
+    ctr = noOfDigits(n);
+
+    printf(" The number of digits in the number is :  %d \n\n",ctr);
+    return 0;
+}
+
+int noOfDigits(int n)
+{
+    static int ctr=0;
+
+     if(n!=0)
+     {
+          ctr++;
+         noOfDigits(n/10);
+    }
+
+    return ctr;
+}
