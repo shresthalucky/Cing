0ccfa8c7c4b7674eaaf841a37da725645192690e
Prashun Chitrakar <prashuncool@gmail.com>
2017-12-08 13:17:38 +0545

    Trying to pass array

--- array/functions.h
+++ array/functions.h
@@ -2,4 +2,6 @@
 #define FUNCTIONS_H_
 
 void aSum(int n, int a[]);
+// void iftrue(int temp2);
+// void iffalse (int temp2,int &a[50]);
 #endif
--- array/main.c
+++ array/main.c
@@ -13,6 +13,8 @@ int main(){
 
   temp2=atoi(n);
 
+	//(temp2==0) ? iftrue(temp2) : iffalse(temp2, &a[50]);
+
 	if(temp2==0){
     printf("Not a interger");
       // exit(EXIT_FAILURE); //same as exit(1)
@@ -24,8 +26,7 @@ int main(){
     		}
     		for (i = 0; i < temp2; i++){
     			for (j=i+1; j < temp2; j++){
-    				if (a[i] > a[j])
-    				{
+    				if (a[i] > a[j]){
     					temp = a[i];
     					a[i] = a[j];
     					a[j] = temp;
@@ -48,3 +49,34 @@ int main(){
 
 	return 0;
 }
+
+// void iftrue(int temp2) {
+// 	printf("Not An Interger");
+// }
+//
+// void iffalse(int temp2, int *a[50]) {
+// 	int i,j,temp;
+// 	for (i = 0; i < temp2; i++){
+// 			printf("Enter number %d: ", i+1);
+// 			scanf("%d", &a[i]);
+// 		}
+// 		for (i = 0; i < temp2; i++){
+// 			for (j=i+1; j < temp2; j++){
+// 				if (a[i] > a[j]){
+// 					temp = a[i];
+// 					a[i] = a[j];
+// 					a[j] = temp;
+// 				}
+// 			}
+// 		}
+//
+// 		printf("Ascending sorting: ");
+// 		for (i = 0; i < temp2; i++){
+// 			printf("%d, ", a[i]);
+// 		}
+//
+// 		printf("Descending sorting: ");
+// 		for (i = temp2-1; i >= 0; i--){
+// 			printf("%d, ", a[i]);
+// 		}
+// }
