81bccba0bb896daf349d44511e29c0b44a23cef9
Prashun Chitrakar <prashuncool@gmail.com>
2017-12-10 20:04:32 +0545

    Different Types of Functions

---
+++ Test/College/header.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_H_
+#define HEADER_H_
+
+void largenum00();
+void largenum01(int,int,int);
+int largenum10();
+int largenum11(int, int, int);
+void checknum1(int, int, int);
+int checknum2(int, int, int);
+int a,b,c,largenum;
+
+#endif
---
+++ Test/College/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+
+int main(){
+  int temp;
+  char n[10];
+  do{
+    printf("What funtion do you want to use? \n1.Type 00\n2.Type 01\n3.Type 10\n4. Type 11\nEnter the value:\t");
+    scanf("%s", n);
+    temp = atoi(n);
+  }while(temp == 0 && temp > 4);
+
+  switch (temp) {
+    case 1 : largenum00();
+    break;
+    case 2 :
+      printf("Enter Three Numbers:\t");
+      scanf("%d %d %d", &a,&b,&c);
+      largenum01(a,b,c);
+      break;
+    case 3 :
+      printf("%d is the largest.", largenum10(a,b,c));
+      break;
+    case 4 :
+      printf("Enter Three Numbers:\t");
+      scanf("%d %d %d", &a,&b,&c);
+      printf("%d is the largest.", largenum11(a,b,c));
+      break;
+    default : printf("Enter Correct Value");
+  }
+  return 0;
+}
+
+void checknum1(int a, int b, int c){
+  if (a>b){
+    if (a>c){
+      printf("%d is the largest.", a);
+    }
+    else{
+      printf("%d is the largest.", c);
+    }
+  }
+  else{
+    printf("%d is the largest.", b);
+  }
+}
+
+int checknum2(int a, int b, int c){
+  if (a>b){
+    if (a>c){
+      return(a);
+    }
+    else{
+      return(c);
+    }
+  }
+  else{
+    return(b);
+  }
+}
+
+void largenum00(){
+    printf("Enter Three Numbers:\t");
+    scanf("%d %d %d", &a,&b,&c);
+    checknum1(a,b,c);
+}
+
+void largenum01(int a,int b,int c){
+  checknum1(a,b,c);
+}
+
+int largenum10(){
+  printf("Enter Three Numbers:\t");
+  scanf("%d %d %d", &a,&b,&c);
+  return checknum2(a,b,c);
+}
+
+int largenum11(int a, int b, int c){
+  return checknum2(a,b,c);
+}
